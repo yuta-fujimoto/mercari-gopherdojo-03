edcda5ad59595b65ad7df97292fb9bfb2c85473d
Fujimoto Yuta <fyuta@c1r13s8.42tokyo.jp>
2022-05-31 23:12:31 +0900

    install jq

--- ex00/api_test.sh
+++ ex00/api_test.sh
@@ -1,13 +1,22 @@
 PORT=4242
 URL=localhost:$PORT
 
+
+# install jq
+which jq > /dev/null
+if [ $? == 1 ]; then
+	brew install jq
+fi
+
+# build
 go build
 
-# back ground exec
+# start server
 ./omikuji $PORT &
 
+# send request
 JSON_RESPONSE=$(curl $URL)
-
 echo $JSON_RESPONSE | jq "."
 
+# end server
 killall omikuji > /dev/null
